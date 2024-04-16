5d412bb8a16d108dbf6fbe51e90a7f76acbd48df
devops <dev0pslearning2025@gmail.com>
2024-04-16 20:20:36 +0530

    Removed all files

---
+++ 3.Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Name1=Ramesh
+Name2=Suresh
+
+echo "Name1: Hello Name2 , How are you? "
+echo "Name2: Name1 i am fine, how about you?"
+echo "Name1:  i am fine too. how is your work?"
+echo "Name2: not bad, i am thinking to upgrade to devops"
