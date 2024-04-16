3e96d65c17b02be8a9a9c6836f81b92e4c61d15d
devops <dev0pslearning2025@gmail.com>
2024-04-16 22:30:34 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-dnf install nginx -y
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+    echo "Please run the script with root access"
+else
+    echo "you are super suer"
+fi
+ dnf install nginx -y
+
+ echo "script is proceeding"
