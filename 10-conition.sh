654d4b711db7f42acd2b2d0c2899bad0a57faa24
devops <dev0pslearning2025@gmail.com>
2024-04-16 22:02:07 +0530

    condition

--- 10-conition.sh
+++ 10-conition.sh
@@ -2,9 +2,9 @@
 
 number=$1
 
-if [ number -gt 10 ] {
+if [ number -gt 10 ] 
 then
     echo "The given number $number is greaterthan 10"
 else
     echo "The given number $number is lessthan 10"
-}
+fi
