866c2363bf9f5a21080c5cbf8e228e3305421822
devopslearning <dev)pslearning2025@gmail.com>
2024-04-16 16:33:08 +0530

    this is my first file

---
+++ 01-steps-git.sh
@@ -0,0 +1,11 @@
+## Steps to git
+
+1. Create a github account\
+2. clone the github to you local laptop
+3. Start deveoping the scripts
+4. add the files to temp dir
+    git add <file name>
+5. commit the files/code
+   git commit -m <write the message for why you are comminting>
+5.Push the code/files to github
+  git push -u origin main --> it will ask github credentials (give the token details)   
