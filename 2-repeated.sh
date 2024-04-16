c6454703dda23e8214a08620388ec14a3edace07
devops <dev0pslearning2025@gmail.com>
2024-04-16 20:18:27 +0530

    Removed all files

---
+++ 2-repeated.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh , How are you? "
+echo "Suresh: Ramesh i am fine, how about you?"
+echo "Ramesh:  i am fine too. how is your work?"
+echo "Suresh: not bad, i am thinking to upgrade to devops"
