cad01707b758b2c985d4bdaa562aeff7397950dd
devops <dev0pslearning2025@gmail.com>
2024-04-16 22:10:35 +0530

    7th file commit

--- 7-testing-variables.sh
+++ 7-testing-variables.sh
@@ -3,4 +3,4 @@
 read -p "Enter username" username
 read -p "enter password" password
 
-echo "the username is $username and the password is $password
+echo "the username is $username and the password is $password"
