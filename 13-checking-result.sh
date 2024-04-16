b298cd9fc05c65573c78e431af138bd0eb9946b2
devops <dev0pslearning2025@gmail.com>
2024-04-16 22:33:53 +0530

    checking installation result

---
+++ 13-checking-result.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+    echo "Please run the script with root access"
+    exit 1 #manuallt exit if error come
+else
+    echo "you are super suer"
+fi
+ dnf install nginxx -y
+
+ if [$? -ne 0]
+ then
+    echo "the nginstallation failure
+fi    
+
+ echo "script is proceeding"
