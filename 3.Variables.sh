13c9e85a5aaab7d64a4ba69c3068664c059107bd
devops <dev0pslearning2025@gmail.com>
2024-04-16 20:31:09 +0530

    Removed all files

--- 3.Variables.sh
+++ 3.Variables.sh
@@ -5,7 +5,7 @@ Name1=Ramesh #no spaces between = and value
 Name2=Suresh
 
 #reffering variables
-echo "Name1: Hello Name2 , How are you? "
-echo "Name2: Name1 i am fine, how about you?"
-echo "Name1:  i am fine too. how is your work?"
-echo "Name2: not bad, i am thinking to upgrade to devops"
+echo "$Name1: Hello $Name2 , How are you? "
+echo "$Name2: $Name1 i am fine, how about you?"
+echo "$Name1:  i am fine too. how is your work?"
+echo "$Name2: not bad, i am thinking to upgrade to devops"
