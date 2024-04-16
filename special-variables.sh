2fea8e06ab6b5f497380f55077dbba749e9eff5d
devops <dev0pslearning2025@gmail.com>
2024-04-16 21:30:54 +0530

    speacial variables

---
+++ special-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All variables: $@"
+echo "number of variables: $#"
+echo "to know the script name; $0"
+echo "current working directory: $PWD"
+echo "Home direcotory of current user: $HOME"
+echo "which user is running the script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "process id of current script: $$"
+
+echo ""
