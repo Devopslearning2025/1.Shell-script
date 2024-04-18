545bc9d3ef2ac704a0f54e74c18ec93c1add000f
devops <dev0pslearning2025@gmail.com>
2024-04-18 12:38:35 +0530

    shell-script

---
+++ 17-colours.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+userid=$(id -u)
+script_name=$(echo $0|cut -d "." -f1)
+date=$(date +%F-%H-%M-%S)
+log=/tmp/$script_name-$date.log
+R=$(\e[31m)
+G=$(\e[32m)
+N=%(\e[0m)
+)
+
+echo "script started at $date"
+
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R failure $N ]"
+        exit 1
+    else
+        echo -e "$2 is $G success $N ]"
+    fi
+}
+
+if [ $userid -ne 0 ]
+then
+    echo " Please run with root user"
+    exit 1
+else
+    echo "you are with root user"    
+fi
+
+dnf install nginx -y &>>$log
+VALIDATE $? "Installing nginx"
+
+dnf install gcc -y &>>$log
+VALIDATE $? "Installing gcc"
