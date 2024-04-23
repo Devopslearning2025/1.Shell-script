cdca26ea1671f07e7d127ea849583219f9465843
devops <dev0pslearning2025@gmail.com>
2024-04-23 16:28:39 +0530

    set -e

---
+++ 20-set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+userid=$(id -u)
+set -e
+if [ $userid -ne 0 ] 
+then
+    echo "Please run the script is with root user"
+    exit 1 #manually exit the script if user is not root
+else
+    echo "you are with root user"
+fi
+
+dnf install gccc -y
+dnf install gzip -y
