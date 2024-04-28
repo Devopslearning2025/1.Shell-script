5b08a9212922277d7027ac95c6851414b19f841c
devops <dev0pslearning2025@gmail.com>
2024-04-28 14:47:46 +0530

    2nd-time

--- 2nd-time/2-prompt-variable.sh
+++ 2nd-time/2-prompt-variable.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Enter 1st person name"
+#echo "Enter 1st person name"
 #read Name1 #its shows the name which you entered 
-read -s Name1 #it doesn't show the name
-echo "Enter 2nd persona name"
+#read -s Name1 #it doesn't show the name
+#echo "Enter 2nd persona name"
 #read Name2  #its shows the name which you entered
-read -s Name2 #it doesn't show the name
+#read -s Name2 #it doesn't show the name
+read -p "Enter 1st person name:" Name1 # which echo comannd
+read -p "Enter 2nd person name:" Name2
 
 echo "$Name1: How are you $Name2"
 echo "$Name2: I am doing good $Name1"
