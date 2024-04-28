05a4451086b45985857ac95a43d95328afa20926
devops <dev0pslearning2025@gmail.com>
2024-04-28 14:55:24 +0530

    2nd-time

--- 2nd-time/3-Arguments.sh
+++ 2nd-time/3-Arguments.sh
@@ -3,8 +3,8 @@
 Name1=$1
 Name2=$2
 
-echo "$Name1: How are you $Name2"
-echo "$Name2: I am doing good $Name1"
-echo "$Name1: how about you $Name2"
-echo "$Name2: I am doing dood, how is you job $Name1"
-echo "$Name1, not well, thisnking to Learn DevOps"
+echo "$Name1 : How are you $Name2"
+echo "$Name2 : I am doing good $Name1"
+echo "$Name1 : how about you $Name2"
+echo "$Name2 : I am doing dood, how is you job $Name1"
+echo "$Name1 : not well, thisnking to Learn DevOps"
