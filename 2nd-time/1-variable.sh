ec2f89921b2f26c3c11f630b231e672608d54bdc
devops <dev0pslearning2025@gmail.com>
2024-04-28 14:42:10 +0530

    2nd-time

---
+++ 2nd-time/1-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Name1=Rakesh
+Name2=Suresh
+
+echo "$Name1: How are you $Name2"
+echo "$Name2: I am doing good $Name1"
+echo "$Name1: how about you $Name2"
+echo "$Name2: I am doing dood, how is you job $Name1"
+echo "$Name1, not well, thisnking to Learn DevOps"
