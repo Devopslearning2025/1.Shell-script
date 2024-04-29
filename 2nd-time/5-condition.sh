dfac6e945208d9bc94355bc25b3664cba2a172f5
devops <dev0pslearning2025@gmail.com>
2024-04-29 20:50:13 +0530

    variables

--- 2nd-time/5-condition.sh
+++ 2nd-time/5-condition.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-NUMBER=$1
+#NUMBER=$1
 
-if [ $NUMBER -gt 10 ]
+#if [ $NUMBER -gt 10 ]
+#then
+ #   echo "the given number $NUMBER is greaterthen 10"
+#else
+#    echo "the given number $NUMBER is lessthan 10"
+#i   
+
+#-gt, -lt, -eq, -ge, -le
+
+if [ -f $1 ]
 then
-    echo "the given number $NUMBER is greaterthen 10"
+    echo "file is exists"
 else
-    echo "the given number $NUMBER is lessthan 10"
-fi   
+    echo "file is there, please create new one"
+fi    
