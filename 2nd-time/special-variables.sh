3adc34c54ae886a1323b0f6aeaee1aebd5bef864
devops <dev0pslearning2025@gmail.com>
2024-04-29 20:37:19 +0530

    variables

---
+++ 2nd-time/special-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "user id: $USER"
+echo "Hostname: $HOSTNAME"
+echo "present working dir: $PWD"
+echo "no of varibles: $#"
+echo "all variables: $@"
+echo "Script name: $0"
+echo "Script PID: $$"
+
+
+echo "last background script PID: $!"
