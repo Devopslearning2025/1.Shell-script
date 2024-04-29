2b8bae7c8997321efc0229b667ff2c6ce24114e8
devops <dev0pslearning2025@gmail.com>
2024-04-29 20:32:40 +0530

    variables

--- 2nd-time/4-Datatypes.sh
+++ 2nd-time/4-Datatypes.sh
@@ -6,13 +6,13 @@
 #Size fo the above is 3
 #The index are 0, 1, 2
 
-#Movies=( "DJ" "Athadu" "RRR" )
+Movies=( "DJ" "Athadu" "RRR" )
 
 #echo "The 1st movie from list is: ${Movies[0]}"
 #echo "The 2nd movie from list is: ${Movies[1]} "
 #echo "The movies are from the list is: ${Movies[@]}"
 
-for i in ( 'DJ' 'Athadu' 'RR' )
+for i in ${Movies[@]}
 do
     echo $i
  #   ((i++))
