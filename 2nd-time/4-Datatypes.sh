d569e774559048785af8aaf5a5231bedf3292e92
devops <dev0pslearning2025@gmail.com>
2024-04-29 20:29:04 +0530

    variables

--- 2nd-time/4-Datatypes.sh
+++ 2nd-time/4-Datatypes.sh
@@ -6,7 +6,7 @@
 #Size fo the above is 3
 #The index are 0, 1, 2
 
-Movies=( "DJ" "Athadu" "RRR" )
+#Movies=( "DJ" "Athadu" "RRR" )
 
 #echo "The 1st movie from list is: ${Movies[0]}"
 #echo "The 2nd movie from list is: ${Movies[1]} "
